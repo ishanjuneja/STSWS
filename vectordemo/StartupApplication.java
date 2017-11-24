e530f66511c822251f93ef2818fb3f37be8397f9
Ishan Juneja <juneja414@gmail.com>
2017-11-24 16:56:35 +0530

    Update StartupApplication.java

--- vectordemo/StartupApplication.java
+++ vectordemo/StartupApplication.java
@@ -2,16 +2,21 @@ package com.yash.main;
 
 import com.yash.model.Collecting;
 import com.yash.model.Person;
-import com.yash.model.SeatReservation;
+import com.yash.service.SeatReservation;
 
 public class StartUpApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SeatReservation seatReservation = new SeatReservation();
-		Person thread1 = new Person(seatReservation);
-		Collecting thread2 = new Collecting(seatReservation);
-		thread1.start();	
+		Collecting thread1 = new Collecting(seatReservation);
+		thread1.setName("ishan");
+		Person thread2 = new Person(seatReservation);
+		thread2.setName("aakash");
+		thread1.start();
 		System.out.println("ishan thread ended");
-		thread2.start();	
+		
+		thread2.start();
+		
+	
 	}
 }
